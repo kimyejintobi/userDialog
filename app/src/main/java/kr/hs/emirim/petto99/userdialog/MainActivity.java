6ed38dfb426a5154cd5285b812bddd40003aa911
kimyejin <kimyejintobi@gmail.com>
2017-05-29 20:55:50 +0900

    #2 scecond commit

--- app/src/main/java/kr/hs/emirim/petto99/userdialog/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/petto99/userdialog/MainActivity.java
@@ -1,13 +1,32 @@
 package kr.hs.emirim.petto99.userdialog;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button but=(Button) findViewById(R.id.but);
+        but.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        View dlgV=View.inflate(this,R.layout.dialog, null);
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setTitle("Input User Information");
+        dialog.setIcon(R.mipmap.zunba);
+        dialog.setView(dlgV);
+        dialog.setPositiveButton("OK",null);
+        dialog.setPositiveButton("CANCEL",null);
+        dialog.show();
     }
 }
